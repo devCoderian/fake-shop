d509255854beb44236fd4b1bfe4c15172379fb85
devCoderian <devCoderian@gmail.com>
2021-10-06 18:19:09 +0900

    모달 따로 빼기

--- front/reducers/cart.js
+++ front/reducers/cart.js
@@ -4,14 +4,7 @@ const initialState = {
         id: 1,
         userId: 'ian'
     },
-    Order:[
-    //     {
-    //     id: null,
-    //     title: null,
-    //     price: null,
-    //     quantity: 0
-    // }
-],
+    Order:[],
 LoadOrderLoading: false,
 LoadOrdertDone: false,
 LoadOrderError: null,
@@ -30,12 +23,6 @@ export const REMOVE_ALL_CART_SUCCEESS = 'REMOVE_ALL_CART_SUCCEESS';
 export const REMOVE_ALL_CART_FAILURE = 'REMOVE_ALL_CART_FAILURE';
 
 
-// export const addCart = (data) => {
-//     return{
-//         type: ADD_CART,
-//         data
-//     }
-// }
 export const addCartAction = (data) => {
     return{
         type: ADD_CART_REQUEST,
@@ -44,93 +31,60 @@ export const addCartAction = (data) => {
 }
 
 export const removeCartAction = (data) => {
-return{
-type: REMOVE_CART_REQUEST,
-data
-}
+    return{
+        type: REMOVE_CART_REQUEST,
+        data
+    }
 }
 
 export const removeAllCartAction = () => {
-return{
-type: REMOVE_ALL_CART_REQUEST
-}
+    return{
+        type: REMOVE_ALL_CART_REQUEST
+    }
 }
 
-// const dummyProduct = {
-//     id: 2,
-//     title: "Mens Casual Premium Slim Fit T-Shirts",
-//     price: 22.3,
-//     image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
-// }
-// const remove = useCallback(
-//     (id) => {
-//         setOrders(orders => {
-//             return orders.filter(order => order.id !== id)
-//         })
-//     },
-//     [],
-// )
-// const removeAll = useCallback(() => {
-//     setOrders([]);
-// },[]);
-
-//Order = Order.filter(order => order.id !== action.data.id)
 
 
 const reducer = (state = initialState, action) => {
 switch(action.type){
-case ADD_CART_REQUEST:{
-   
-    return{
-        ...state,
-        LoadOrderLoading: true,
-        LoadOrdertDone: false,
-    }
-}
-case ADD_CART_SUCCEESS:{
-    // console.log('처음',action.data);
-    // }
-    let checkList = [...state.Order]
-    const check = checkList.find(order =>order.id === action.data.id);
-    console.log('check',check);
-    if(check === undefined){
-        console.log('처음');
-            // action.data.quantity = 1;
+    
+    case ADD_CART_REQUEST:{
+    
         return{
             ...state,
-            Order: [action.data, ...state.Order],
-            LoadOrderLoading: false,
-            LoadOrdertDone: true
+            LoadOrderLoading: true,
+            LoadOrdertDone: false,
         }
-    }else{
-        console.log('중복');
-        checkList.map((order)=>{
-            if(check.id === order.id){
-                order.quantity++;
-                return{
-                    ...state,
-                    Order: [...state.Order],
-                    LoadOrderLoading: false,
-                    LoadOrdertDone: true
-                    }
+    }
+
+    case ADD_CART_SUCCEESS:{
+
+        let checkList = [...state.Order];
+
+        const check = checkList.find(order =>order.id === action.data.id);
+
+        if(check === undefined){
+            return{
+                ...state,
+                Order: [action.data, ...state.Order],
+                LoadOrderLoading: false,
+                LoadOrdertDone: true
             }
-          })
+        }else{
+            checkList.map((order)=>{
+                if(check.id === order.id){
+                    order.quantity++;
+                    return{
+                        ...state,
+                        Order: [...state.Order],
+                        LoadOrderLoading: false,
+                        LoadOrdertDone: true
+                        }
+                }
+            })
       }
-        // console.log('중복')
-        // let copy = [...state.Order]
-        // copy[found].quantity+= action.data.quantity
-        // console.log(copy)
-        // return{
-        //     ...state,
-        //     ...state.Order[found].quantity++,
-        //     // Order: [...state.Order],
-        //     Order: [...state.Order],
-           
-        //     LoadOrderLoading: false,
-        //     LoadOrdertDone: true
-        // }
-  
 }
+
 case REMOVE_CART_REQUEST:{
     return{
         ...state
@@ -138,9 +92,11 @@ case REMOVE_CART_REQUEST:{
 }
 
 case REMOVE_CART_SUCCEESS:{
+
     let Order = [...state.Order];
+    
     Order.filter(order => order.id !== action.data);
-    console.log('remove',Order);
+
     return{
         ...state,
         Order:Order.filter(order => order.id !== action.data)
